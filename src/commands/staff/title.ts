add2cdc9a0b5482da778dc5c9d08d9b090ad0290
KingOKarma <virtualdisaster782@gmail.com>
2021-12-16 16:52:39 +0000

    Staff command updates

--- src/commands/staff/title.ts
+++ src/commands/staff/title.ts
@@ -2,17 +2,16 @@ import { Commands } from "../../interfaces/commands";
 
 export const command: Commands = {
     // Note aliases are optional
-    aliases: ["p"],
     cooldown: 3,
     cooldownResponse: "Hey! just wait like {time}",
-    description: "Test if the bot is running and its response time!",
-    example: ["!title We've been streaming for {mt} minuites!", "!title Stream time {h} hours and {m} minuites!", "!title Streaming for {time}!"],
-    group: "info",
+    description: "Tells you if you're staff or not!",
+    example: ["!checkstaff"],
+    group: "staff",
     modOnly: true,
-    name: "ping",
+    name: "checkstaff",
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     run: async(client, msg, args) => {
         // Run your code here
-        return client.say(msg.params.target, "Pong! 🏓");
+        return client.say(msg.params.target, "You are staff! congratz!");
     }
 };
